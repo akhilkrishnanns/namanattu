ed402870110d733a2b9ab2a408364308362bd7b5
akhilkrishnanns <akhilkrishnanns@live.com>
2019-12-08 00:25:31 +0530

    test run commit. home and about completed.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <main-app></main-app>
+  </div>
+</template>
+
+<script>
+import MainApp from '@/components/MainApp.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MainApp
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+} */
+@import'~bootstrap/dist/css/bootstrap.css'
+</style>
